451ee4563b6c9c6f2511e09a50e8ddd949b5219c
Muzikayise Melusi Mthunzi <MthunziM@ukzn.ac.za>
2022-07-18 09:56:06 +0200

    second commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,8 +44,5 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function setPasswordAttribute($value)
-    {
-          $this->attributes['password'] = bcrypt($value);
-     }
+   
 }
