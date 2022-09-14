81ccdc748a3014f426609408a782e4f25fbe21f5
Sibongiseni Msomi <19819312+sibowilldo@users.noreply.github.com>
2022-09-14 10:15:07 +0200

    Added Model Accessor Methods
    Accessor Methods or Model Attributes, make it possible to alter the data before it sent to the user. The user cases on these models are a good example.

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -52,6 +53,20 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * The User model should always load organization
+     *
+     * @var string[]
+     */
+    protected $with = ['organization'];
+
+    public function fullname(): Attribute
+    {
+        return Attribute::make(
+            get: fn() => $this->first_name . ' ' . $this->last_name
+        );
+    }
 
     public function organization(): BelongsTo
     {
