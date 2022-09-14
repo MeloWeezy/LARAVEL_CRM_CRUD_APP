3401cb42f44f95df14842075317e5501e3653a90
Sibongiseni Msomi <19819312+sibowilldo@users.noreply.github.com>
2022-09-14 10:23:45 +0200

    Update web.php
    Remove duplicate code
    User Route::Resource where possible. Route::Resource() has methods ->only([]) & ->except([]) read more about these in the Laravel Docs

--- routes/web.php
+++ routes/web.php
@@ -42,16 +42,13 @@ Route::middleware(['auth','role:super_admin'])
         Route::resource('/permissions', PermissionController::class);
         Route::post('/permissions/{permission}/roles', [PermissionController::class, 'assignRole'])->name('permissions.roles');
         Route::delete('/permissions/{permission}/roles/{role}', [PermissionController::class, 'removeRole'])->name('permissions.roles.remove');
-        Route::get('/users', [UserController::class, 'index'])->name('users.index');
-        Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
-        Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
-        Route::post('/users/{user}/roles', [UserController::class, 'assignRole'])->name('users.roles');
-        Route::delete('/users/{user}/roles/{role}', [UserController::class, 'removeRole'])->name('users.roles.remove');
-        Route::post('/users/{user}/permissions', [UserController::class, 'givePermission'])->name('users.permissions');
-        Route::delete('/users/{user}/permissions/{permission}', [UserController::class, 'revokePermission'])->name('users.permissions.revoke');
-        Route::get('/users', [UsersController::class, 'index'])->name('users.index');
-        Route::get('/users/{user}', [UsersController::class, 'show'])->name('users.show');
-        Route::delete('/users/{user}', [UsersController::class, 'destroy'])->name('users.destroy');
+
+        Route::resource('/users', UsersController::class);
+
+        Route::post('/users/{user}/roles', [UsersController::class, 'assignRole'])->name('users.roles');
+        Route::delete('/users/{user}/roles/{role}', [UsersController::class, 'removeRole'])->name('users.roles.remove');
+        Route::post('/users/{user}/permissions', [UsersController::class, 'givePermission'])->name('users.permissions');
+        Route::delete('/users/{user}/permissions/{permission}', [UsersController::class, 'revokePermission'])->name('users.permissions.revoke');
     }
 );
 
@@ -62,8 +59,8 @@ Route::middleware(['auth','role:super_admin'])
     Route::resource('organizations', OrganizationsController::class);
     Route::resource('users', UsersController::class);
     Route::resource('Dashboard', DashboardController::class);
-    
-    
+
+
 });
 
 
